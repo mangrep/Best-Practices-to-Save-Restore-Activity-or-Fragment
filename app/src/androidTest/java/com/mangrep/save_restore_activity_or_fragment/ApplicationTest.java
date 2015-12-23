4a883c9163a6bebaa14d9f3e20953e6435bcff66
manish <manish.choudhary@livquik.com>
2015-12-23 18:30:50 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/mangrep/save_restore_activity_or_fragment/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mangrep.save_restore_activity_or_fragment;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

8e1da596aea0aeff2f3b3c41dfe001da51e46ebe
manish <manishchoudhary55@gmail.com>
2015-12-23 19:00:26 +0530

    Changes to save class variable

--- app/src/main/java/com/mangrep/save_restore_activity_or_fragment/MainActivity.java
+++ app/src/main/java/com/mangrep/save_restore_activity_or_fragment/MainActivity.java
@@ -1,15 +1,17 @@
 package com.mangrep.save_restore_activity_or_fragment;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = MainActivity.class.getName();
+
+    //These variable will destroyed with Activity
+    private String someVariable1;
+    private String someVariable2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        someVariable1 = "var1";
+        someVariable2 = "var2";
     }
 
+    /*
+     *View will be automatically saved by android. Just save the class variables
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putString("someVariable1", someVariable1);
+        outState.putString("someVariable2", someVariable2);
     }
 
+    /*
+     *get saved variables
+     */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "someVariable1 = " + someVariable1 + " someVariable2 = " + someVariable2);
+        someVariable1 = savedInstanceState.getString("someVariable1");
+        someVariable2 = savedInstanceState.getString("someVariable2");
     }
 }
